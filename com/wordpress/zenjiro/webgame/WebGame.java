ca5f55ff4bd91c2facab4911ccd0cbf5fa50fa52
zenjiro <zenjiro0123@gmail.com>
2011-08-27 17:47:58 +0900

    1枚目をクリックしてみる

--- com/wordpress/zenjiro/webgame/WebGame.java
+++ com/wordpress/zenjiro/webgame/WebGame.java
@@ -1,6 +1,9 @@
 package com.wordpress.zenjiro.webgame;
 
+import java.awt.AWTException;
 import java.awt.Point;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
 
 /**
  * Web Gameを解く
@@ -9,8 +12,18 @@ public class WebGame {
 	/**
 	 * メインメソッド
 	 * @param args コマンドライン引数
+	 * @throws AWTException AWT例外
+	 * @throws InterruptedException 割り込み例外
 	 */
-	public static void main(final String[] args) {
+	public static void main(final String[] args) throws AWTException, InterruptedException {
+		// 1枚目をクリックしてみる
+		Thread.sleep(2000);
+		final Robot robot = new Robot();
+		final Point point = getLocation(0, 0);
+		robot.mouseMove(point.x, point.y);
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		Thread.sleep(100);
+		robot.mouseRelease(InputEvent.BUTTON1_MASK);
 	}
 
 	/**
@@ -18,7 +31,7 @@ public class WebGame {
 	 * @param col 列
 	 * @return カードの位置
 	 */
-	public static Point getLocation(int row, int col) {
+	public static Point getLocation(final int row, final int col) {
 		return new Point(40, 70);
 	}
 }

7672612aeaaf7c748c0a56ae8443fe509875693c
zenjiro <zenjiro0123@gmail.com>
2011-08-28 08:56:51 +0900

    取得したデータを元にシートを作ってデータを追加

--- AppsScript.js
+++ AppsScript.js
@@ -1,14 +1,17 @@
 function myFunction() {
   var spreadSheet = SpreadsheetApp.getActiveSpreadsheet();
-  if (spreadSheet.getSheetByName("てすと") == null) {
-    spreadSheet.insertSheet("てすと");
-  }
-  var sheet = spreadSheet.getSheetByName("てすと");
-  sheet.clear();
- 
   var object = Utilities.jsonParse(UrlFetchApp.fetch("gdd-2011-quiz-japan.appspot.com/apps_script/sample").getContentText());
   for (var i in object) {
     var city = object[i];
-    sheet.appendRow([city]);
+    var city_name = city.city_name;
+    if (spreadSheet.getSheetByName(city_name) == null) {
+      spreadSheet.insertSheet(city_name);
+    }
+    var sheet = spreadSheet.getSheetByName(city_name);
+    sheet.clear();
+    for (var j in city.data) {
+      var record = city.data[j];
+      sheet.appendRow([record.capacity, record.usage]);
+    }
   }
 }

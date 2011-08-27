e8990644417d0683c3332d5077eff3d95a1b7de6
zenjiro <zenjiro0123@gmail.com>
2011-08-27 17:10:49 +0900

    WebGameのスタブを作成

---
+++ com/wordpress/zenjiro/webgame/WebGame.java
@@ -0,0 +1,13 @@
+package com.wordpress.zenjiro.webgame;
+
+/**
+ * Web Gameを解く
+ */
+public class WebGame {
+	/**
+	 * メインメソッド
+	 * @param args コマンドライン引数
+	 */
+	public static void main(final String[] args) {
+	}
+}

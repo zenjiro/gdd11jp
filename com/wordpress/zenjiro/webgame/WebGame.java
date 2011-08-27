854cdfec0a22857c94b8eac9024a04f0e70f1623
zenjiro <zenjiro0123@gmail.com>
2011-08-27 21:46:19 +0900

    またカードの枚数が増えたのでパラメータを調整。これでクリアした。

--- com/wordpress/zenjiro/webgame/WebGame.java
+++ com/wordpress/zenjiro/webgame/WebGame.java
@@ -18,38 +18,38 @@ public class WebGame {
 	 */
 	public static void main(final String[] args) throws AWTException, InterruptedException {
 		Thread.sleep(2000);
-		final int rows = 32;
+		final int rows = 128;
 		final int cols = 8;
-		final boolean[][] isDone = new boolean[rows][cols];
-		final Color[][] colors = new Color[rows][cols];
-		final Robot robot = new Robot();
-		for (int row = 0; row < colors.length; row++) {
-			for (int column = 0; column < colors[row].length; column++) {
-				final Point point = getLocation2(row, column);
-				if (robot.getPixelColor(point.x, point.y).equals(new Color(221, 221, 221))) {
-					click(point, robot);
-					colors[row][column] = robot.getPixelColor(point.x, point.y);
-				} else {
-					isDone[row][column] = true;
+		for (int count = 0; count < 5; count++) {
+			final boolean[][] isDone = new boolean[rows][cols];
+			final Color[][] colors = new Color[rows][cols];
+			final Robot robot = new Robot();
+			for (int row = 0; row < colors.length; row++) {
+				for (int column = 0; column < colors[row].length; column++) {
+					final Point point = getLocation3(row, column);
+					if (robot.getPixelColor(point.x, point.y).equals(new Color(221, 221, 221))) {
+						click(point, robot);
+						colors[row][column] = robot.getPixelColor(point.x, point.y);
+					} else {
+						isDone[row][column] = true;
+					}
 				}
 			}
-		}
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				if (!isDone[i][j]) {
-					inner: for (int k = 0; k < rows; k++) {
-						for (int l = 0; l < cols; l++) {
-							if (!isDone[k][l]) {
-								if (i * cols + j < k * cols + l) {
-									if (colors[i][j].equals(colors[k][l])) {
-										System.out.printf("[%d, %d] == [%d, %d]: %s\n", i, j, k, l,
-												colors[i][j]);
-										click(getLocation2(i, j), robot);
-										click(getLocation2(k, l), robot);
-										break inner;
-									} else {
-										System.out.printf("[%d, %d] (%s) != [%d, %d] (%s): \n", i,
-												j, colors[i][j], k, l, colors[k][l]);
+			Thread.sleep(2000);
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < cols; j++) {
+					if (!isDone[i][j]) {
+						inner: for (int k = 0; k < rows; k++) {
+							for (int l = 0; l < cols; l++) {
+								if (!isDone[k][l]) {
+									if (i * cols + j < k * cols + l) {
+										if (colors[i][j].equals(colors[k][l])) {
+											System.out.printf("[%d, %d] == [%d, %d]: %s\n", i, j,
+													k, l, colors[i][j]);
+											click(getLocation3(i, j), robot);
+											click(getLocation3(k, l), robot);
+											break inner;
+										}
 									}
 								}
 							}
@@ -57,6 +57,7 @@ public class WebGame {
 					}
 				}
 			}
+			Thread.sleep(10000);
 		}
 	}
 
@@ -76,6 +77,7 @@ public class WebGame {
 	}
 
 	/**
+	 * Chrome 100%
 	 * @param row 行
 	 * @param col 列
 	 * @return カードの位置
@@ -85,11 +87,22 @@ public class WebGame {
 	}
 
 	/**
+	 * Firefox 30%
 	 * @param row 行
 	 * @param col 列
 	 * @return カードの位置
 	 */
 	public static Point getLocation2(final int row, final int col) {
 		return new Point(15 + col * 25, (int) (30 + row * 24.6));
+	}
+
+	/**
+	 * Firefox 5%
+	 * @param row 行
+	 * @param col 列
+	 * @return カードの位置
+	 */
+	public static Point getLocation3(final int row, final int col) {
+		return new Point(3 + col * 4, 10 + (int) (row * 5.2));
 	}
 }

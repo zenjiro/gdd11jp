085d49bd8e777f8b731bc54ad261a6dc60349c4b
zenjiro <zenjiro0123@gmail.com>
2011-08-28 10:41:35 +0900

    各問題も読み取って表示してみる

--- com/wordpress/zenjiro/slidingpuzzle/SlidingPuzzle.java
+++ com/wordpress/zenjiro/slidingpuzzle/SlidingPuzzle.java
@@ -16,18 +16,21 @@ public class SlidingPuzzle {
 	 */
 	public static void main(final String[] args) throws IOException {
 		final Scanner scanner = new Scanner(SlidingPuzzle.class.getResourceAsStream("problems.txt"));
+		scanner.useDelimiter("[\\s,]");
 		int lx = scanner.nextInt();
 		int rx = scanner.nextInt();
 		int ux = scanner.nextInt();
 		int dx = scanner.nextInt();
 		int n = scanner.nextInt();
-		scanner.nextLine();
 		Logger.getLogger(SlidingPuzzle.class.getName()).log(Level.INFO,
 				"lx = {0}, rx = {1}, ux = {2}, dx = {3}, n = {4}",
 				new Integer[] { lx, rx, ux, dx, n });
-		while (scanner.hasNextLine()) {
-			final String line = scanner.nextLine();
-			System.out.println(line);
+		while (scanner.hasNext()) {
+			int w = scanner.nextInt();
+			int h = scanner.nextInt();
+			String b = scanner.next();
+			Logger.getLogger(SlidingPuzzle.class.getName()).log(Level.INFO,
+					"w = {0}, h = {1}, b = {2}", new Object[] { w, h, b });
 		}
 		scanner.close();
 	}

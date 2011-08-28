6ba0a5a278709bb3377670d66d4fdeb33c2e0227
zenjiro <zenjiro0123@gmail.com>
2011-08-28 22:00:20 +0900

    とりあえず壁を無視

--- com/wordpress/zenjiro/slidingpuzzle/Util.java
+++ com/wordpress/zenjiro/slidingpuzzle/Util.java
@@ -5,15 +5,19 @@ package com.wordpress.zenjiro.slidingpuzzle;
  */
 public class Util {
 	/**
-	 * "ABC"を"16,17,18"に変換します。
+	 * "ABC"を"16,17,18"に変換します。変換できない文字は、その位置に対応する数字に変換します。
 	 * @param hex 16進表記
 	 * @return 10進表記
 	 */
 	public static String hexToDecimal(String hex) {
 		StringBuilder builder = new StringBuilder();
 		for (int i = 0; i < hex.length(); i++) {
-			int value = Integer.parseInt(hex.substring(i, i + 1), 16);
-			builder.append(value + ",");
+			try {
+				int value = Integer.parseInt(hex.substring(i, i + 1), 16);
+				builder.append(value + ",");
+			} catch (NumberFormatException e) {
+				builder.append((i + 1) + ",");
+			}
 		}
 		return builder.toString().replaceFirst(",$", "");
 	}

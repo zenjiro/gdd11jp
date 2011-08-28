05de8dd5a6eebc4e0fc2a14224752ed843501906
zenjiro <zenjiro0123@gmail.com>
2011-08-28 10:21:46 +0900

    SlidingPuzzleのスタブを作成

---
+++ com/wordpress/zenjiro/slidingpuzzle/SlidingPuzzle.java
@@ -0,0 +1,13 @@
+package com.wordpress.zenjiro.slidingpuzzle;
+
+/**
+ * スライドパズルを解く
+ */
+public class SlidingPuzzle {
+	/**
+	 * メインメソッド
+	 * @param args コマンドライン引数
+	 */
+	public static void main(final String[] args) {
+	}
+}

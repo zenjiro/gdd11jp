f7bc2c9e2cf80e819d3cc2b638f6cf3869a55e47
zenjiro <zenjiro0123@gmail.com>
2011-08-28 10:26:34 +0900

    問題文を読み込んでそのまま表示するだけ

--- com/wordpress/zenjiro/slidingpuzzle/SlidingPuzzle.java
+++ com/wordpress/zenjiro/slidingpuzzle/SlidingPuzzle.java
@@ -1,5 +1,9 @@
 package com.wordpress.zenjiro.slidingpuzzle;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 /**
  * スライドパズルを解く
  */
@@ -7,7 +11,14 @@ public class SlidingPuzzle {
 	/**
 	 * メインメソッド
 	 * @param args コマンドライン引数
+	 * @throws IOException 入出力例外
 	 */
-	public static void main(final String[] args) {
+	public static void main(final String[] args) throws IOException {
+		final Scanner scanner = new Scanner(SlidingPuzzle.class.getResourceAsStream("problems.txt"));
+		while (scanner.hasNextLine()) {
+			final String line = scanner.nextLine();
+			System.out.println(line);
+		}
+		scanner.close();
 	}
 }

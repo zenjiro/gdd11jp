ab9f631f81a59b3ba871b15b5caa273358e83a22
zenjiro <zenjiro0123@gmail.com>
2011-08-28 10:32:58 +0900

    問題文のメタ情報を読み込んで表示してみる

--- com/wordpress/zenjiro/slidingpuzzle/SlidingPuzzle.java
+++ com/wordpress/zenjiro/slidingpuzzle/SlidingPuzzle.java
@@ -2,6 +2,8 @@ package com.wordpress.zenjiro.slidingpuzzle;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * スライドパズルを解く
@@ -14,6 +16,15 @@ public class SlidingPuzzle {
 	 */
 	public static void main(final String[] args) throws IOException {
 		final Scanner scanner = new Scanner(SlidingPuzzle.class.getResourceAsStream("problems.txt"));
+		int lx = scanner.nextInt();
+		int rx = scanner.nextInt();
+		int ux = scanner.nextInt();
+		int dx = scanner.nextInt();
+		int n = scanner.nextInt();
+		scanner.nextLine();
+		Logger.getLogger(SlidingPuzzle.class.getName()).log(Level.INFO,
+				"lx = {0}, rx = {1}, ux = {2}, dx = {3}, n = {4}",
+				new Integer[] { lx, rx, ux, dx, n });
 		while (scanner.hasNextLine()) {
 			final String line = scanner.nextLine();
 			System.out.println(line);

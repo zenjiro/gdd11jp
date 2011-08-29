452ef6afb49f0cd7cdf84bc0538a8a91091ea84f
zenjiro <zenjiro0123@gmail.com>
2011-08-29 21:49:31 +0900

    とりあえず初期状態を表示してみる。

--- com/wordpress/zenjiro/slidingpuzzle/BluteForceSolver.java
+++ com/wordpress/zenjiro/slidingpuzzle/BluteForceSolver.java
@@ -6,6 +6,7 @@ package com.wordpress.zenjiro.slidingpuzzle;
 public class BluteForceSolver implements Solver {
 	@Override
 	public String solve(final int w, final int h, final String b, final long limitMillis) {
+		Util.print(w, h, b);
 		return null;
 	}
 }

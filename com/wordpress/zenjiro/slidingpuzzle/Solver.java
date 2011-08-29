ba11b9ea78318616389c4301afb28338b3c91d24
zenjiro <zenjiro0123@gmail.com>
2011-08-29 21:39:22 +0900

    メソッドのスタブを作成

--- com/wordpress/zenjiro/slidingpuzzle/Solver.java
+++ com/wordpress/zenjiro/slidingpuzzle/Solver.java
@@ -4,4 +4,15 @@ package com.wordpress.zenjiro.slidingpuzzle;
  * 自作ソルバ
  */
 public class Solver {
+	/**
+	 * スライドパズルを解きます。
+	 * @param w ボードの幅
+	 * @param h ボードの高さ
+	 * @param b ボード初期状態
+	 * @param limitMillis 制限時間[ms]
+	 * @return 手順
+	 */
+	public String solve(final int w, final int h, final String b, final int limitMillis) {
+		return null;
+	}
 }

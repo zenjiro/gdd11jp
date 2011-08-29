d114331ce7eac103b950df5b668b5c8372c4fad7
zenjiro <zenjiro0123@gmail.com>
2011-08-29 21:55:45 +0900

    移動の向きを表す列挙型を追加

--- com/wordpress/zenjiro/slidingpuzzle/Const.java
+++ com/wordpress/zenjiro/slidingpuzzle/Const.java
@@ -4,4 +4,25 @@ package com.wordpress.zenjiro.slidingpuzzle;
  * 定数を集めたクラス
  */
 public class Const {
+	/**
+	 * 移動の向き
+	 */
+	public enum Direction {
+		/**
+		 * 左
+		 */
+		LEFT,
+		/**
+		 * 右
+		 */
+		RIGHT,
+		/**
+		 * 上
+		 */
+		UP,
+		/**
+		 * 下
+		 */
+		DOWN
+	}
 }

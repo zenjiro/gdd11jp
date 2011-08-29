b48b5205ead13a0a2b78be99074856fb689e3368
zenjiro <zenjiro0123@gmail.com>
2011-08-29 21:43:18 +0900

    総当りのソルバのスタブを追加

---
+++ com/wordpress/zenjiro/slidingpuzzle/BluteForceSolver.java
@@ -0,0 +1,11 @@
+package com.wordpress.zenjiro.slidingpuzzle;
+
+/**
+ * 総当りのソルバ
+ */
+public class BluteForceSolver implements Solver {
+	@Override
+	public String solve(final int w, final int h, final String b, final long limitMillis) {
+		return null;
+	}
+}

666f4dd56e655f568a2be426cf9593b9eb07ea4e
zenjiro <zenjiro0123@gmail.com>
2011-08-31 23:38:54 +0900

    今までの結果をマージしたら2.98点に下がった。不正解に正解が上書きされ他っぽい。

---
+++ com/wordpress/zenjiro/slidingpuzzle/Merge.java
@@ -0,0 +1,41 @@
+package com.wordpress.zenjiro.slidingpuzzle;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * 回答をマージするツール
+ */
+public class Merge {
+	/**
+	 * メインメソッド
+	 * @param args コマンドライン引数
+	 * @throws FileNotFoundException ファイル未検出例外
+	 */
+	public static void main(final String[] args) throws FileNotFoundException {
+		final PrintWriter out = new PrintWriter(Merge.class.getPackage().getName()
+				.replace(".", "/")
+				+ "/output.txt");
+		final String[] results = new String[5000];
+		for (int i = 1; i < 11; i++) {
+			final Scanner scanner = new Scanner(
+					Merge.class.getResourceAsStream(Integer.toString(i)));
+			for (int j = 0; j < 5000; j++) {
+				final String line = scanner.nextLine();
+				if (results[j] == null) {
+					results[j] = line;
+				} else {
+					if (line.length() > 0 && line.length() < results[j].length()) {
+						results[j] = line;
+					}
+				}
+			}
+			scanner.close();
+		}
+		for (final String line : results) {
+			out.println(line);
+		}
+		out.close();
+	}
+}

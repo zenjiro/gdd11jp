d47619ac39a1c88d74397583b3529369109be5bd
zenjiro <zenjiro0123@gmail.com>
2011-09-03 22:05:08 +0900

    NodeをComparableに。equalsも実装。

--- com/wordpress/zenjiro/slidingpuzzle/BruteForceSolver.java
+++ com/wordpress/zenjiro/slidingpuzzle/BruteForceSolver.java
@@ -12,7 +12,7 @@ public class BruteForceSolver implements Solver {
 	/**
 	 * ノード
 	 */
-	static class Node {
+	static class Node implements Comparable<Node> {
 		/**
 		 * ボードの幅
 		 */
@@ -29,6 +29,10 @@ public class BruteForceSolver implements Solver {
 		 * ここまでの手順
 		 */
 		final String path;
+		/**
+		 * ヒューリスティック距離
+		 */
+		final int heuristic;
 
 		/**
 		 * ノードを初期化します。
@@ -36,46 +40,69 @@ public class BruteForceSolver implements Solver {
 		 * @param h ボードの高さ
 		 * @param b ボードの状態
 		 * @param path ここまでの手順
+		 * @param heuristic ヒューリスティック距離
 		 */
-		public Node(final int w, final int h, final String b, final String path) {
+		public Node(final int w, final int h, final String b, final String path, final int heuristic) {
 			this.w = w;
 			this.h = h;
 			this.b = b;
 			this.path = path;
+			this.heuristic = heuristic;
 		}
 
 		@Override
 		public String toString() {
-			return this.b + ", " + this.path;
+			return this.b + ", " + this.path + ", " + this.heuristic;
+		}
+
+		@Override
+		public int compareTo(final Node node) {
+			if (this.heuristic == node.heuristic) {
+				return this.path.length() - node.path.length();
+			} else {
+				return this.heuristic - node.heuristic;
+			}
+		}
+
+		@Override
+		public boolean equals(final Object object) {
+			if (object instanceof Node) {
+				return this.path.equals(((Node) object).path);
+			} else {
+				return false;
+			}
 		}
 	}
 
 	@Override
-	public String solve(final int w, final int h, String b, final long limitMillis) {
+	public String solve(final int w, final int h, final String b, final long limitMillis) {
 		final long startTimeMillis = System.currentTimeMillis();
 		final String goal = Util.getGoal(b);
 		final String startB = b;
-		Node currentNode = new Node(w, h, b, "");
+		Node currentNode = new Node(w, h, b, "", Util.getHeuristicDistance(w, h, b));
 		final Queue<Node> queue = new ArrayDeque<Node>();
-		System.out.println("current = " + currentNode + ", heuristic = "
-				+ Util.getHeuristicDistance(w, h, currentNode.b));
 		while (!currentNode.b.equals(goal)
 				&& System.currentTimeMillis() - startTimeMillis < limitMillis) {
+			System.out.println("current = " + currentNode);
 			final String left = Util.move(Direction.LEFT, w, h, currentNode.b);
 			if (left != null && !left.equals(startB)) {
-				queue.add(new Node(w, h, left, currentNode.path + "L"));
+				queue.add(new Node(w, h, left, currentNode.path + "L", Util.getHeuristicDistance(w,
+						h, left)));
 			}
 			final String right = Util.move(Direction.RIGHT, w, h, currentNode.b);
 			if (right != null && !right.equals(startB)) {
-				queue.add(new Node(w, h, right, currentNode.path + "R"));
+				queue.add(new Node(w, h, right, currentNode.path + "R", Util.getHeuristicDistance(
+						w, h, right)));
 			}
 			final String up = Util.move(Direction.UP, w, h, currentNode.b);
 			if (up != null && !up.equals(startB)) {
-				queue.add(new Node(w, h, up, currentNode.path + "U"));
+				queue.add(new Node(w, h, up, currentNode.path + "U", Util.getHeuristicDistance(w,
+						h, up)));
 			}
 			final String down = Util.move(Direction.DOWN, w, h, currentNode.b);
 			if (down != null && !down.equals(startB)) {
-				queue.add(new Node(w, h, down, currentNode.path + "D"));
+				queue.add(new Node(w, h, down, currentNode.path + "D", Util.getHeuristicDistance(w,
+						h, down)));
 			}
 			currentNode = queue.remove();
 		}

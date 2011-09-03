c91f69174848a732eb879654d612c8df012d74fd
zenjiro <zenjiro0123@gmail.com>
2011-09-03 12:14:11 +0900

    問題文と回答を読み込んでみる。

--- com/wordpress/zenjiro/slidingpuzzle/Check.java
+++ com/wordpress/zenjiro/slidingpuzzle/Check.java
@@ -1,5 +1,9 @@
 package com.wordpress.zenjiro.slidingpuzzle;
 
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * 結果を検算するツール
  */
@@ -9,5 +13,35 @@ public class Check {
 	 * @param args コマンドラン引数
 	 */
 	public static void main(final String[] args) {
+		{
+			final Scanner scanner = new Scanner(Check.class.getResourceAsStream("problems.txt"));
+			scanner.useDelimiter("[\\s,]");
+			final int lx = scanner.nextInt();
+			final int rx = scanner.nextInt();
+			final int ux = scanner.nextInt();
+			final int dx = scanner.nextInt();
+			final int n = scanner.nextInt();
+			Logger.getLogger(Check.class.getName()).log(Level.INFO,
+					"lx = {0}, rx = {1}, ux = {2}, dx = {3}, n = {4}",
+					new Integer[] { lx, rx, ux, dx, n });
+			while (scanner.hasNext()) {
+				final int w = scanner.nextInt();
+				final int h = scanner.nextInt();
+				final String b = scanner.next();
+				Logger.getLogger(Check.class.getName()).log(Level.INFO,
+						"w = {0}, h = {1}, b = {2}", new Object[] { w, h, b });
+			}
+			scanner.close();
+		}
+		final String[] results = new String[5000];
+		{
+			final Scanner scanner = new Scanner(Check.class.getResourceAsStream("output.txt"));
+			for (int i = 0; i < 5000; i++) {
+				final String line = scanner.nextLine();
+				results[i] = line;
+				Logger.getLogger(Check.class.getName()).log(Level.INFO, "result = {0}", line);
+			}
+			scanner.close();
+		}
 	}
 }

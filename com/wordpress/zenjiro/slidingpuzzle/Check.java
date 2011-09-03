44c9a84354718fee7dd942b824ab75d7356802d6
zenjiro <zenjiro0123@gmail.com>
2011-09-03 12:05:52 +0900

    結果を検算するツールのスタブを作成

---
+++ com/wordpress/zenjiro/slidingpuzzle/Check.java
@@ -0,0 +1,13 @@
+package com.wordpress.zenjiro.slidingpuzzle;
+
+/**
+ * 結果を検算するツール
+ */
+public class Check {
+	/**
+	 * メインメソッド
+	 * @param args コマンドラン引数
+	 */
+	public static void main(final String[] args) {
+	}
+}

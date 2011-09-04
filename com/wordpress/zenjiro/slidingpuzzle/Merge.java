76c2cfbe45ed976d00cc547a617f5bb03dce2bc2
zenjiro <zenjiro0123@gmail.com>
2011-09-04 18:58:16 +0900

    ランダムに解いた結果同士をマージしたら663問解けた！

--- com/wordpress/zenjiro/slidingpuzzle/Merge.java
+++ com/wordpress/zenjiro/slidingpuzzle/Merge.java
@@ -1,5 +1,7 @@
 package com.wordpress.zenjiro.slidingpuzzle;
 
+import java.io.File;
+import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.Scanner;
@@ -18,8 +20,13 @@ public class Merge {
 				.replace(".", "/")
 				+ "/output.txt");
 		final String[] results = new String[5000];
-		for (final String file : new String[] { "bruteforce-10000.txt", "4.8" }) {
-			final Scanner scanner = new Scanner(Merge.class.getResourceAsStream(file));
+		for (final File file : new File(".").listFiles(new FileFilter() {
+			@Override
+			public boolean accept(final File file) {
+				return file.getName().matches("output[-0-9]+.txt");
+			}
+		})) {
+			final Scanner scanner = new Scanner(file);
 			for (int i = 0; i < 5000; i++) {
 				final String line = scanner.nextLine();
 				if (results[i] == null) {

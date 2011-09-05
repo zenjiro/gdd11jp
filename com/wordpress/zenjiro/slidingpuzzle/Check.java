20bb193c76232781ec061832a5ede500d18a7407
zenjiro <zenjiro0123@gmail.com>
2011-09-05 12:39:10 +0900

    不要なimport文を削除

--- com/wordpress/zenjiro/slidingpuzzle/Check.java
+++ com/wordpress/zenjiro/slidingpuzzle/Check.java
@@ -3,7 +3,6 @@ package com.wordpress.zenjiro.slidingpuzzle;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Formatter;
 import java.util.List;
 import java.util.Scanner;

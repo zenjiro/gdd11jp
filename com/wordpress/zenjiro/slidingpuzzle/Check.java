ed53cc1154afc06f6833174faf7861a0bd4b62e2
zenjiro <zenjiro0123@gmail.com>
2011-09-10 00:05:48 +0900

    MAGIC=3。解答：582/5000（11.6%）L：11120/72187（15.4%）R：12283/81749（15.0%）U：11085/72303（15.3%）D：12305/81778（15.0%）

--- com/wordpress/zenjiro/slidingpuzzle/Check.java
+++ com/wordpress/zenjiro/slidingpuzzle/Check.java
@@ -55,9 +55,9 @@ public class Check {
 	 * @throws FileNotFoundException ファイル未検出例外
 	 */
 	public static void main(final String[] args) throws FileNotFoundException {
-		final String inputFile = "merged.txt";
+		final String inputFile = "output.txt";
 		final String outputFile = "checked.txt";
-		final int pathLimit = 215;
+		final int pathLimit = 215000;
 		final List<Board> problems = new ArrayList<Board>();
 		final int[][] counts = new int[7][7];
 		final Scanner problemsScanner = new Scanner(Check.class.getResourceAsStream("problems.txt"));
